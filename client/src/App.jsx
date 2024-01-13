d29c540d54119a1e8067eb7c1e5551f1259c1df4
Parasaram Naidu Tentu <tpn_the_great@Parasarams-MacBook-Pro.local>
2024-01-14 01:59:35 +0530

    router done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,30 @@
 import React from "react";
 import "./App.css";
 import Home from "./pages/Home/Home";
+import Feed from "./Components/FeedPage/Feed";
 import Explore from "./pages/Explore/Explore";
 import Notifications from "./pages/Notifications/Notifications";
 import Messages from "./pages/Messages/Messages";
 import Lists from "./pages/Lists/Lists";
 import Bookmarks from "./pages/Bookmarks/Bookmarks";
 import Profile from "./pages/Profile/Profile";
-import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
-    Children: [
+    children: [
+      {
+        index: true,
+        element: <Feed />,
+      },
       {
         path: "explore",
         element: <Explore />,
       },
       {
-        path: "notification",
+        path: "notifications",
         element: <Notifications />,
       },
       {
@@ -45,9 +50,7 @@ const router = createBrowserRouter([
 const App = () => {
   return (
     <RouterProvider router={router}>
-      <div className="app">
-        <Outlet />
-      </div>
+      <div className="app"></div>
     </RouterProvider>
   );
 };
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,16 +1,13 @@
 import React from "react";
-import Feed from "../../Components/FeedPage/Feed";
+import "./Home.css";
 import SideNavBar from "../../Components/SideNavBar/SideNavBar";
 import Widgets from "../../Components/Widgets/Widgets";
-
-import "./Home.css";
 import { Outlet } from "react-router-dom";
 
 const Home = () => {
   return (
     <div className="app">
       <SideNavBar />
-      <Feed />
       <Outlet />
       <Widgets />
     </div>
