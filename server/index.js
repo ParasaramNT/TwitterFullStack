285edec89769dc3c454b7f90138116447d09b2bf
ParasaramNT <tentusantosh9@gmail.com>
2024-01-23 10:33:31 +0530

    chanding cors

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://twitter-backend-gbfe.onrender.com"],
     credentials: true,
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   })
